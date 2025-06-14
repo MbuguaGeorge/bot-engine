92fd9a028f7f2955ef2d9041b8f6f4e22f5d05d0
MbuguaGeorge <mbuguageorge026@gmail.com>
2025-06-14 07:38:10 +0300

    admin page fix

--- bots/admin.py
+++ bots/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bot
 
-# Register your models here.
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'phone_number', 'status', 'whatsapp_connected', 'created_at', 'last_updated')
+    search_fields = ('name', 'user__email', 'phone_number')
+    list_filter = ('status', 'whatsapp_connected', 'created_at', 'last_updated')
--- flows/admin.py
+++ flows/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Flow
 
-# Register your models here.
+@admin.register(Flow)
+class FlowAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bot', 'status', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('name', 'bot__name')
+    list_filter = ('status', 'is_active', 'created_at', 'updated_at')
