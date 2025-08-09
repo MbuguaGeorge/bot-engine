badaf55fc7643513b8926155e9b76b204ed46f3e
MbuguaGeorge <mbuguageorge026@gmail.com>
2025-08-09 08:52:22 +0300

    deploy

--- API/settings.py
+++ API/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/opt/wozza/API/staticfiles/'
 
 # Media files that users upload
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/opt/wozza/API/media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
